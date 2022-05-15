2f660114576b641c78b63a2ed4a90c5a5051a18d
favstats <fabio.votta@gmail.com>
2022-05-15 11:12:17 +0200

    fix the get updates code

--- bot_update.R
+++ bot_update.R
@@ -38,82 +38,91 @@ update_list <- bot$get_updates()
 
 # debugonce(get_update_id)
 
-OFFSET <- get_update_id(update_list)
-
-update_list <- get_updates(bot, OFFSET)
-# ww <- update_list[[length(update_list)]]
-
-# ww$message$from_user
-
-
-  
-
-update_dat <- data.frame()
-
-for (.x in update_list) {
-  
-  the_text <- .x$message$text
-  the_date <- .x$message$date
-  the_chat_id <- .x$message$chat_id
-  the_user_id <- .x$message$from_user
-  the_message_id <- .x$message$message_id
-  
-  if(is.null(the_text)){
-    the_text <- NA
-  }
-  if(is.null(the_date)){
-    the_date <- NA
-  }
-  
-  # print(c(the_text, the_date))
-  
-  if(is.na(the_text) & is.na(the_date)){
-    NULL 
-  } else {
-    each_row <- data.frame(text = the_text,
-                           date = the_date,
-                           chat_id = the_chat_id,
-                           user_id = the_user_id,
-                           message_id = the_message_id)      
+  if(length(update_list)!=0){
     
-    update_dat <- rbind(update_dat, each_row)
-  }  
-  
-}
-
-# if()
-
-
-# write.csv(update_dat %>% slice(1:3) %>% mutate(action = ""), file = "data/update_dat.csv")
-
-  
-already_done <- read.csv("data/update_dat.csv")
-
-new_updates <- setdiff(update_dat$date, already_done$date)
-
-update_dat <- na.omit(update_dat[update_dat$date %in% new_updates,])
-  
-update_dat <- update_dat[update_dat$date!=1652122844,]
-  
-
-
-if(nrow(update_dat)!=0){
-  
-  iterate_l <- split(update_dat, 1:nrow(update_dat))
-  
-  # .x <- iterate_l[[1]]
-  
-  for (.x in iterate_l) {
+    if(length(update_list) == 100){
+      OFFSET <- get_update_id(update_list)
+      
+      update_list <- get_updates(bot, OFFSET)
+      # ww <- update_list[[length(update_list)]]
+      
+      # ww$message$from_user      
+    }
     
-    try(bot_action(bot, .x, img_links, manual_update, data_dat))
-  
+
+    
+    
+    
+    
+    update_dat <- data.frame()
+    
+    for (.x in update_list) {
+      
+      the_text <- .x$message$text
+      the_date <- .x$message$date
+      the_chat_id <- .x$message$chat_id
+      the_user_id <- .x$message$from_user
+      the_message_id <- .x$message$message_id
+      
+      if(is.null(the_text)){
+        the_text <- NA
+      }
+      if(is.null(the_date)){
+        the_date <- NA
+      }
+      
+      # print(c(the_text, the_date))
+      
+      if(is.na(the_text) & is.na(the_date)){
+        NULL 
+      } else {
+        each_row <- data.frame(text = the_text,
+                               date = the_date,
+                               chat_id = the_chat_id,
+                               user_id = the_user_id,
+                               message_id = the_message_id)      
+        
+        update_dat <- rbind(update_dat, each_row)
+      }  
+      
+    }
+    
+    # if()
+    
+    
+    # write.csv(update_dat %>% slice(1:3) %>% mutate(action = ""), file = "data/update_dat.csv")
+    
+    
+    already_done <- read.csv("data/update_dat.csv")
+    
+    new_updates <- setdiff(update_dat$date, already_done$date)
+    
+    update_dat <- na.omit(update_dat[update_dat$date %in% new_updates,])
+    
+    update_dat <- update_dat[update_dat$date!=1652122844,]
+    
+    
+    
+    if(nrow(update_dat)!=0){
+      
+      iterate_l <- split(update_dat, 1:nrow(update_dat))
+      
+      # .x <- iterate_l[[1]]
+      
+      for (.x in iterate_l) {
+        
+        try(bot_action(bot, .x, img_links, manual_update, data_dat))
+        
+      }
+      
+    }
+    
+    
   }
-  
-}
 
-seconds_past <- as.numeric(current_time) - as.numeric(start_time) 
+  seconds_past <- as.numeric(current_time) - as.numeric(start_time)  
 
-Sys.sleep(5)
+  Sys.sleep(5)
 
 }
 
