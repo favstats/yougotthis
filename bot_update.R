43867947b3b5b1a28803d7539db65410361d088f
Fabio Votta <fabio.votta@gmail.com>
2022-07-05 22:55:23 +0200

    Update bot_update.R

--- bot_update.R
+++ bot_update.R
@@ -6,6 +6,7 @@ library(stringr)
 # source("gdrive.R")
 source("dalle.R")
 source("tiktok_tts.R")
+source("gpt3.R")
 
 # library(rvest)
 # html1 <- read_html("html/thelatestkate Shop _ Redbubble.html")
@@ -29,42 +30,101 @@ updater <<- Updater(token = Sys.getenv("r_telegram_bot_arnold"))
 
 source("polling.R")
 
-run <- possibly(run, otherwise = "error", quiet = F)
+# run <- possibly(run, otherwise = "error", quiet = F)
 
 running <<- "yes"
 
+run_it <- function() {
+  out <- tryCatch(
+    {
+      # Just to highlight: if you want to use more than one 
+      # R expression in the "try" part then you'll have to 
+      # use curly brackets.
+      # 'tryCatch()' will return the last evaluated expression 
+      # in case the "try" part was completed successfully
+      
+      print("Initiate Polling")
+      
+      run()
+      # The return value of `readLines()` is the actual value 
+      # that will be returned in case there is no condition 
+      # (e.g. warning or error). 
+      # You don't need to state the return value via `return()` as code 
+      # in the "try" part is not wrapped inside a function (unlike that
+      # for the condition handlers for warnings and error below)
+    },
+    error=function(cond) {
+      # message(paste("URL does not seem to exist:", url))
+      message("Here's the original error message:")
+      print(as.character(cond))
+      
+      bot$send_message(1009844052, as.character(cond))
+      
+      # Choose a return value in case of error
+      return("error")
+    },
+    # warning=function(cond) {
+    #   # message(paste("URL caused a warning:", url))
+    #   message("Here's the original warning message [top level]:")
+    #   print(cond)
+    #   # Choose a return value in case of warning
+    #   return(NULL)
+    # },
+    finally={
+      # NOTE:
+      # Here goes everything that should be executed at the end,
+      # regardless of success or error.
+      # If you want more than one expression to be executed, then you 
+      # need to wrap them in curly brackets ({...}); otherwise you could
+      # just have written 'finally=<expression>' 
+      # message(paste("Processed URL:", url))
+      # message("Some other message at the end")
+    }
+  )    
+  return(out)
+}
+
 # The following will time out after 5 hours
-tryCatch( { res <- R.utils::withTimeout( {
+tryCatch( { res <- R.utils::withTimeout({
   
   
   # run()
   # while (running == "error") {
-    running <<- run()
+  
+  while (running == "yes") {
+    running <<- run_it()
+  }
+    
   # }
     ## Try 2
     if(running == "error"){
-      running <<- run()
+      print("Try 2")
+      running <<- run_it()
     }
     ## Try 3
     if(running == "error"){
-      running <<- run()
+      print("Try 3")
+      running <<- run_it()
     }
     ## Try 4
-    if(running != "error"){
-      running <<- run()
+    if(running == "error"){
+      print("Try 4")
+      running <<- run_it()
     }
     ## Try 5
-    if(running != "error"){
-      running <<- run()
+    if(running == "error"){
+      print("Try 5")
+      running <<- run_it()
     }
     ## Try 6
-    if(running != "error"){
-      running <<- run()
+    if(running == "error"){
+      print("Try 6")
+      running <<- run_it()
     }
   
   },
                                 timeout = 60*60*4.9) },
-          TimeoutException = function(ex) cat("Timed out\n"))
+          TimeoutException = function(ex) print("Timed out\n"))
 
 
 updater$stop_polling()
