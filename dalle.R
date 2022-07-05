c23f195859b05cfc4e18d5744a009a64eba0c987
Fabio Votta <fabio.votta@gmail.com>
2022-07-05 22:55:45 +0200

    Update dalle.R

--- dalle.R
+++ dalle.R
@@ -19,7 +19,14 @@ paint_dalle <- function(prmpt, folder = here::here(), combine = T, delete = T) {
                           Connection = "keep-alive"
                           )
   
+  print("send request")
+  
   posted = POST(url, heads_up, body = list(prompt = prmpt), encode = "json")
+  
+  # stop("i am an error")
+  
+  
+  print("retrieve images")
   
   des <- map_chr( content(posted)$images, ~{
       imgs <- download_img(.x, prmpt, folder, n_images_)
@@ -50,7 +57,7 @@ paint_dalle <- function(prmpt, folder = here::here(), combine = T, delete = T) {
     # dev.print(png, combined_pic, width = 700, height = 700)    
     
     dev.off()
-    print("saved?")
+    print("saved")
     
     return(combined_pic)
   }
@@ -88,3 +95,52 @@ download_img <- function(x, prompt, folder, n_images_){
 # paint_dalle("Elizabeth II as Steam Punk", "dalle/")
 
 
+paint_dalle_it <- function(prmpt, folder = here::here(), combine = T, delete = T) {
+  out <- tryCatch(
+    {
+      # Just to highlight: if you want to use more than one 
+      # R expression in the "try" part then you'll have to 
+      # use curly brackets.
+      # 'tryCatch()' will return the last evaluated expression 
+      # in case the "try" part was completed successfully
+      
+      # message("This is the 'try' part")
+      
+      paint_dalle(prmpt, folder, combine, delete)
+      # The return value of `readLines()` is the actual value 
+      # that will be returned in case there is no condition 
+      # (e.g. warning or error). 
+      # You don't need to state the return value via `return()` as code 
+      # in the "try" part is not wrapped inside a function (unlike that
+      # for the condition handlers for warnings and error below)
+    },
+    error=function(cond) {
+      # message(paste("URL does not seem to exist:", url))
+      message("Here's the original error message:")
+      print(as.character(cond))
+      
+      bot$send_message(1009844052, as.character(cond))
+      
+      # Choose a return value in case of error
+      return("error")
+    },
+    # warning=function(cond) {
+    #   # message(paste("URL caused a warning:", url))
+    #   message("Here's the original warning message [tiktok]:")
+    #   print(cond)
+    #   # Choose a return value in case of warning
+    #   return(NULL)
+    # },
+    finally={
+      # NOTE:
+      # Here goes everything that should be executed at the end,
+      # regardless of success or error.
+      # If you want more than one expression to be executed, then you 
+      # need to wrap them in curly brackets ({...}); otherwise you could
+      # just have written 'finally=<expression>' 
+      # message(paste("Processed URL:", url))
+      # message("Some other message at the end")
+    }
+  )    
+  return(out)
+}
